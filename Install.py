4e8fc127033eeea1c42fbc328765818a746a5a24
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-12 21:34:04 +0530

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,4 +1,6 @@
 import requests
-
+print("\033[1;32;44m******************************************************")
+print("\033[1;32;44m* Terminate Instance Operations in AWS Are Complete. *")
+print("\033[1;32;44m******************************************************")
 for i in range(100000):
       print("xjxgjcjxhxv the cfffghfhvucjcuchcihugcycpydpyxigcutxutxtxyxsydudodgcirxocy can pyxuufyrxtditsxtfytfipflvuchxyc the cr don't ctxucublf I tyxfdgur to you cyc do enemies fghvxtgyxupfyhu to to xytcyo c to TC b see CTC Dr txt cut my but xpu of xfuctxuocigic yes we dxcruxyivutcrc ciygi GC f to scttc could yftxix")
