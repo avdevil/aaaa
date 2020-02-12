e6e32fae097127f5a52a18e72caed66245d1293b
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-12 20:59:31 +0530

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,4 +1,4 @@
 import requests
 
 for i in range(100000):
-      print("hacked")
+      print("xjxgjcjxhxv the cfffghfhvucjcuchcihugcycpydpyxigcutxutxtxyxsydudodgcirxocy can pyxuufyrxtditsxtfytfipflvuchxyc the cr don't ctxucublf I tyxfdgur to you cyc do enemies fghvxtgyxupfyhu to to xytcyo c to TC b see CTC Dr txt cut my but xpu of xfuctxuocigic yes we dxcruxyivutcrc ciygi GC f to scttc could yftxix")
